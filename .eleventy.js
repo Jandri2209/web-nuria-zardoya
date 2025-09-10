86999a37b73ad1bea9e0b57dde640eb182fa8ca8
Alejandro <alostado@das-nano.com>
2025-09-10 14:59:04 +0200

    Ajuste de best practice 2

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,8 @@ module.exports = function(eleventyConfig) {
     "favicon-16x16.png": "favicon-16x16.png",
     "favicon-32x32.png": "favicon-32x32.png",
     "favicon-48x48.png": "favicon-48x48.png",
+    "favicon-64x64.png": "favicon-64x64.png",
+    "favicon-96x96.png": "favicon-96x96.png",
     "apple-touch-icon.png": "apple-touch-icon.png",
     "android-chrome-192x192.png": "android-chrome-192x192.png",
     "android-chrome-512x512.png": "android-chrome-512x512.png",
